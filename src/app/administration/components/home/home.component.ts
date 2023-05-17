894cb74a9da1f7d5059705f6157a08151bcce09c
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 15:04:46 +0300

    🎨 improvement (admin): create home component

---
+++ src/app/administration/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  currentRoute!: string;
+
+  constructor(public activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.currentRoute = this.activatedRoute?.snapshot?.url[0]?.path;
+  }
+}
