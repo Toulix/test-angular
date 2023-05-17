a5617da30a2bebfb5656b58138f1abec7535c0ef
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 13:48:46 +0300

    ✨ feat (auth): do not allow unauthorized acces to the app

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from 'src/app/core/service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const userInfo = this.authService.getUserInfoFromLocalStorage();
+
+    if (userInfo) return true;
+
+    return this.router.createUrlTree(['/authentication']);
+  }
+}
