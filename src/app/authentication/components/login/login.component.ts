025b193a61bfede57835c43bd3f7fc1fa3bca4aa
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 13:44:38 +0300

    🎨 improvement (auth): create input validations for login form

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from './../../../core/service/auth.service';
+import { DEFAULT_PASSWORD } from 'src/app/constants/constants';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,27 +13,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
+      username: [null, [Validators.required]],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern(DEFAULT_PASSWORD),
+        ],
+      ],
     });
   }
 
   submitForm(): void {
-    if (this.loginForm.valid) {
-      const { userName, password } = this.loginForm.value;
-
-      this.router.navigate(['/administration/home']);
-    } else {
-      Object.values(this.loginForm.controls).forEach((control) => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        }
-      });
-    }
+    // if (this.loginForm.valid) {
+    //   const { username, password } = this.loginForm.value;
+    //   this.authService.login({ username, password });
+    //   this.router.navigate(['/administration/home']);
+    // } else {
+    //   Object.values(this.loginForm.controls).forEach((control) => {
+    //     if (control.invalid) {
+    //       control.markAsDirty();
+    //       control.updateValueAndValidity({ onlySelf: true });
+    //     }
+    //   });
+    // }
   }
 }
