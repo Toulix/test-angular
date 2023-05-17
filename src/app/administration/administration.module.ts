16cece3173106189b205d117836ce8c22bc6e413
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 17:59:39 +0300

    🎨 improvement (admin): import missing module

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgZorroAntdModule } from '../ng-zorro-antd.module';
 import { AdministrationRoutingModule } from './administration-routing.module';
@@ -8,6 +9,7 @@ import { AdministrationComponent } from './administration.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TeamsComponent } from './components/teams/teams.component';
+import { TeamdetailsComponent } from './components/TeamDetails/teamdetails.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,13 @@ import { TeamsComponent } from './components/teams/teams.component';
     HomeComponent,
     HeaderComponent,
     TeamsComponent,
+    TeamdetailsComponent,
   ],
-  imports: [CommonModule, AdministrationRoutingModule, NgZorroAntdModule],
+  imports: [
+    CommonModule,
+    AdministrationRoutingModule,
+    NgZorroAntdModule,
+    HttpClientModule,
+  ],
 })
 export class AdministrationModule {}
