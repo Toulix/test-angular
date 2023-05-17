80d9f8e795428a1528e337e272d559fe65aa0099
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 13:45:46 +0300

    🎨 improvement (auth): create login service

---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private route: Router) {}
+
+  login(userCredentials: User) {
+    // send http request to verify credentials... (should return back a token)
+    const { username, password } = userCredentials;
+    // http resquest...
+
+    // test@gmail.com is a dummy email address since we don't have an api for now
+    const userData = {
+      username,
+      email: 'test@gmail.com',
+      token: 'loremipsumsitdoloramset',
+    };
+    localStorage.setItem('user', JSON.stringify(userData));
+  }
+}
