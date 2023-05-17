c87c7575f8fecfa9d5badb693e15942d18c48d37
Toulix <tolojanaharypatrickandre@gmail.com>
2023-05-17 11:58:00 +0300

    ✨ feat: create login component

---
+++ src/app/authentication/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  submitForm(): void {
+    if (this.loginForm.valid) {
+      const { userName, password } = this.loginForm.value;
+
+      this.router.navigate(['/administration/home']);
+    } else {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
